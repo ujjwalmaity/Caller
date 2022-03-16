dcb8fdec2d5dbd2b92aba9d911f717ea561416b7
ujjwal <ujjwalmaity98@gmail.com>
2022-03-16 12:29:10 +0530

    Fetching caller number

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:caller/caller.dart';
 import 'package:flutter/material.dart';
 import 'screen_home.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  initialize();
+
   runApp(const MyApp());
 }
 
@@ -20,5 +25,39 @@ class MyApp extends StatelessWidget {
         ScreenHome.id: (context) => const ScreenHome(),
       },
     );
+  }
+}
+
+/// Defines a callback that will handle all background incoming events
+///
+/// The duration will only have a value if the current event is `CallerEvent.callEnded`
+Future<void> callerCallbackHandler(CallerEvent event, String number, int? duration) async {
+  debugPrint("New event received from native $event");
+  switch (event) {
+    case CallerEvent.callEnded:
+      debugPrint('[ Caller ] Ended a call with number $number and duration $duration');
+      break;
+    case CallerEvent.onMissedCall:
+      debugPrint('[ Caller ] Missed a call from number $number');
+      break;
+    case CallerEvent.onIncomingCallAnswered:
+      debugPrint('[ Caller ] Accepted call from number $number');
+      break;
+    case CallerEvent.onIncomingCallReceived:
+      debugPrint('[ Caller ] Phone is ringing with number $number');
+      break;
+  }
+}
+
+Future<void> initialize() async {
+  /// Check if the user has granted permissions
+  final permission = await Caller.checkPermission();
+  debugPrint('Caller permission $permission');
+
+  /// If not, then request user permission to access the Call State
+  if (!permission) {
+    Caller.requestPermissions();
+  } else {
+    Caller.initialize(callerCallbackHandler);
   }
 }
